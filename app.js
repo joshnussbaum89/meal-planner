bf38f86376c53b791744afa133db64a6c2d48bcd
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-10-17 16:51:34 -0400

    build getDay and getRandomWeeklyMeals functions

--- app.js
+++ app.js
@@ -5,17 +5,76 @@ const tagline = document.querySelector('.tagline');
 const startButton = document.getElementById('startButton');
 const planDiv = document.querySelector('.plan');
 const mealList = document.getElementById('meals');
-const name = 'Josh' // Assign your name
-const meals = []; // Add in some meals...
+const name = 'Josh';
+let mealPlan = [];
+const meals = [
+    'Beef Teriyaki with Pineapple',
+    'Vegan Quesadilla',
+    'Oven Roasted Broccoli Heads with Halloumi',
+    'Spiced Pear and Goat Cheese Salad',
+    'Spaghetti with Tomato Sauce',
+    'Bacon and Onion Sandwich',
+    'California Sushi Roll',
+    'Loaded Nachos',
+    'Mozzarella and Tomato Salad',
+    'Chicken and Waffles',
+    'Beef and Shrimp Lo Mein',
+    'Tofu Feta, Watermelon and Chicken Salad',
+    'Barbecue Spare Ribs',
+    'Healthy Veggie Curry with Garlic Naan'
+];
 
-// HELPER FUNCTIONS
-const getDay = () => { };
+// Get current day of the week
+function getDay() {
+    const date = new Date();
+    let currentDay = date.getDay();
 
-const getRandomWeeklyMeals = () => { };
+    switch (currentDay) {
+        case 0:
+            currentDay = "Sunday";
+            break;
+        case 1:
+            currentDay = "Monday";
+            break;
+        case 2:
+            currentDay = "Tuesday";
+            break;
+        case 3:
+            currentDay = "Wednesday";
+            break;
+        case 4:
+            currentDay = "Thursday";
+            break;
+        case 5:
+            currentDay = "Friday";
+            break;
+        case 6:
+            currentDay = "Saturday";
+            break;
+    }
+    return currentDay;
+}
 
-const displayWeeklyMealPlan = () => { };
+// Get random number and create new array to store weekly meal plan
+function getRandomWeeklyMeals() {
 
-const showPlan = () => { };
+    for (let i = 0; i < 7; i++) {
+        const randomNumber = Math.floor(Math.random() * meals.length);
+        mealPlan.push(meals[randomNumber]);
+    }
+
+    return mealPlan;
+}
+
+getRandomWeeklyMeals();
+
+function displayWeeklyMealPlan() {
+
+}
+
+function showPlan() {
+
+}
 
 headline.textContent = getDay();
 tagline.textContent = `Hey ${name}, click the button below to see your weekly meal planner.`;
